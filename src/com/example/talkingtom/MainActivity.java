534a6dc5f4641d6532bc499f1a7e7e35090b1b28
Zlatin.Zlatev <Zlatin.Zlatev@bulpros.com>
2014-08-07 09:44:50 +0300

    Made MainActivity the entry point of the project

---
+++ src/com/example/talkingtom/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.talkingtom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
